d2a1dc78c3fd0538c62b20e61054e5381fb5e884
Megan Fayer and Tal Haberer <student@epicodus.com>
2016-01-21 14:35:43 -0800

    add js functions and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
+//Business Logic
 
+var pingPong = function(num){
+
+  for (var i = 1; i <= num; i++) {
+    if (i % 15 === 0) {
+      return "ping-pong";
+    } else if (i % 3 === 0) {
+      return "ping";
+    } else if (i % 5 === 0) {
+      return "pong"
+    } else {
+      return i;
+    };
+  };
+};
+
+
+
+
+
+//User Interface Logic
 $(document).ready(function(){
   $("form").submit(function(event){
     var num = $('#input-number').val()
